4cf5b9d5559c895853d326a00e5896c659c24f69
johnphilipsprague <jpsprague95@gmail.com>
2022-05-13 06:16:41 -0500

    resolving 2nd merge conflicts from dev

--- source/js/scenes/BootScene.js
+++ source/js/scenes/BootScene.js
@@ -2,7 +2,6 @@ import * as Phaser from "../phaser.js"
 import keys from "./keys"
 
 class BootScene extends Phaser.Scene {
-<<<<<<< HEAD
 	constructor() {
 		super({ key: keys.BOOT_SCENE })
 	}
@@ -41,7 +40,7 @@ class BootScene extends Phaser.Scene {
 
 		// our two characters
 		this.load.atlas(
-			"battleButz",
+			"battleMort",
 			"../../../public/MORT/MORT/BattleMortSpritesheet.png",
 			"../../../public/MORT/MORT/BattleMortSpritesheet.json"
 		)
@@ -54,12 +53,12 @@ class BootScene extends Phaser.Scene {
 
 		// butz overworld atlas
 		this.load.atlas(
-			"playerButz",
+			"playerMort",
 			"../../../public/MORT/MORT/OverworldMortSpritesheet.png",
 			"../../../public/MORT/MORT/OverworldMortSpritesheet.json"
 		)
 
-		// Enemies
+		//enemies
 
 		this.load.image("whiteWolf", "../../../public/MORT/ENEMIES/10.png")
 
@@ -73,73 +72,6 @@ class BootScene extends Phaser.Scene {
 	create() {
 		this.scene.start(keys.START_SCENE)
 	}
-=======
-  constructor() {
-    super({ key: keys.BOOT_SCENE });
-  }
-  preload() {
-    // map tiles
-    this.load.image(
-      "tiles",
-      "../../../public/MORT/MAPS/Tilesets/Tileset 7.png"
-    );
-
-    this.load.image(
-      "floor1Tiles",
-      "../../../public/MORT/MAPS/Tilesets/Tileset 6+7.png"
-    );
-
-    this.load.image(
-      "finalFloorTiles",
-      "../../../public/MORT/MAPS/Tilesets/Tileset 1.png"
-    );
-
-    // map in json format
-    this.load.tilemapTiledJSON(
-      "map",
-      "../../../public/MORT/MAPS/OverworldMapV2.json"
-    );
-
-    this.load.tilemapTiledJSON(
-      "floor1",
-      "../../../public/MORT/MAPS/Tower1V2.json"
-    );
-
-    this.load.tilemapTiledJSON(
-      "FinalTowerFloor",
-      "../../../public/MORT/MAPS/FinalFloor.json"
-    );
-
-    // our two characters
-    this.load.atlas(
-      "battleMort",
-      "../../../public/MORT/MORT/BattleMortSpritesheet.png",
-      "../../../public/MORT/MORT/BattleMortSpritesheet.json"
-    );
-
-    this.load.atlas(
-      "skeleman",
-      "../../../public/MORT/SKELEMAN/SkeleSpritesheet.png",
-      "../../../public/MORT/SKELEMAN/SkeleSpritesheet.json"
-    );
-
-    this.load.image("goblin", "../../../public/MORT/ENEMIES/5.png");
-
-    // butz overworld atlas
-    this.load.atlas(
-      "playerMort",
-      "../../../public/MORT/MORT/OverworldMortSpritesheet.png",
-      "../../../public/MORT/MORT/OverworldMortSpritesheet.json"
-    );
-
-    // white wolf enemy
-    this.load.image("whiteWolf", "../../../public/MORT/ENEMIES/10.png");
-  }
-
-  create() {
-    this.scene.start(keys.START_SCENE);
-  }
->>>>>>> 07964005ebcdf3e148792669c3caebdd3a7ace3c
 }
 
 export default BootScene
