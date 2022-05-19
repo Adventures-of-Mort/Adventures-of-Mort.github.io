1e2e59e2c80b802c406733035920c81f178e57a4
Eric Jiang <eyjiang18@gmail.com>
2022-05-19 00:42:02 -0400

    changed boot back to start

--- source/js/scenes/BootScene.js
+++ source/js/scenes/BootScene.js
@@ -104,7 +104,7 @@ class BootScene extends Phaser.Scene {
   create() {
     this.registry.set("context", context);
 
-    this.scene.start(keys.BOSS_SCENE);
+    this.scene.start(keys.START_SCENE);
   }
 }
 
